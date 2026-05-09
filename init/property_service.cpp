e169fda241dac9f111cc991c742e57dde9994407
Pranav Vashi <neobuddy89@gmail.com>
2026-05-10 00:26:11 +0530

    Revert "init: property_service: Hide adbd"
    This reverts commit 49ac5eb46166b7d2bc373ed25b0d1de9254b6027.
    
    Fixes: https://github.com/crdroidandroid/issue_tracker/issues/980
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- init/property_service.cpp
+++ init/property_service.cpp
@@ -1246,7 +1246,6 @@ static void SetSafetyNetProps() {
         {"ro.is_ever_orange", "0"},
         {"ro.secureboot.devicelock", "1"},
         {"ro.secureboot.lockstate", "locked"},
-        {"init.svc.adbd", "stopped"},
     };
 
     for (const auto& [name, value] : props) {
